d0fe2bcee7f23939bef9bd2ee4c9a19be47e8739
brickgao <brickgao@gmail.com>
2015-04-24 09:47:15 +0800

    add 'Multiply String'

---
+++ Multiply_Strings.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+
+class Solution:
+    # @param {string} num1
+    # @param {string} num2
+    # @return {string}
+    def multiply(self, num1, num2):
+        return str(int(num1) * int(num2))
