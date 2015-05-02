e276e7b8c1f84f85cac219122976df6f2a05cae9
brickgao <brickgao@gmail.com>
2015-05-02 11:35:32 +0800

    add 'Unique Paths'

---
+++ src/Unique_Paths.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+
+class Solution:
+    # @param {integer} m
+    # @param {integer} n
+    # @return {integer}
+    def uniquePaths(self, m, n):
+        dp = [[0 for i in range(n)] for j in range(m)]
+        for i in range(m):
+            dp[i][0] = 1
+        for i in range(n):
+            dp[0][i] = 1
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+        return dp[m - 1][n - 1]
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    print solution.uniquePaths(3, 7)
