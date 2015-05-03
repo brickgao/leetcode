e248f50d3bf554df7553d0f03182459b3e2de3ec
brickgao <brickgao@gmail.com>
2015-05-03 13:37:03 +0800

    add 'Minimum Path Sum'

---
+++ src/Minimum_Path_Sum.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+
+class Solution:
+    # @param {integer[][]} grid
+    # @return {integer}
+    def minPathSum(self, grid):
+        m, n = len(grid), len(grid[0])
+        dp = [[0 for i in range(n)] for j in range(m)]
+        dp[0][0] = grid[0][0]
+        for i in range(1, m):
+            dp[i][0] = grid[i][0] + dp[i - 1][0]
+        for i in range(1, n):
+            dp[0][i] = grid[0][i] + dp[0][i - 1]
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j]
+        return dp[m - 1][n - 1]
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    print solution.minPathSum([[1, 3, 1],
+                               [1, 5, 1],
+                               [4, 2, 1]])
