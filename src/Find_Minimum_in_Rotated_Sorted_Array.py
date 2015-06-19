1e548c473a543525e0b071baa05183ff2fa62ad6
brickgao <brickgao@gmail.com>
2015-06-19 23:12:26 +0800

    add 'Find Minimum in Rotated Sorted Array'

---
+++ src/Find_Minimum_in_Rotated_Sorted_Array.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+
+class Solution:
+    # @param {integer[]} nums
+    # @return {integer}
+    def findMin(self, nums):
+        l, r = 0, len(nums) - 1
+        while l < r:
+            mid = (l + r) >> 1
+            if nums[l] <= nums[mid] <= nums[r]:
+                return nums[l]
+            elif nums[l] >= nums[mid] >= nums[r]:
+                return nums[r]
+            else:
+                if nums[mid] < nums[l]:
+                    r = mid
+                else:
+                    l = mid
+        return nums[l]
+        
+if __name__ == "__main__":
+    solution = Solution()
+    print solution.findMin([4, 5, 6, 7, 0, 1, 2])
+    print solution.findMin([1])
