b26c1a6db865e93dadad1dcb8557435180105ed6
brickgao <brickgao@gmail.com>
2015-08-16 15:30:45 +0800

    add 'Remove Element'

---
+++ src/algorithms/cpp/Remove_Element.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <cstdlib>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <map>
+#include <set>
+using namespace std;
+#define out(v) cerr << #v << ": " << (v) << endl
+#define SZ(v) ((int)(v).size())
+const int maxint = -1u>>1;
+template <class T> bool get_max(T& a, const T &b) {return b > a? a = b, 1: 0;}
+template <class T> bool get_min(T& a, const T &b) {return b < a? a = b, 1: 0;}
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int i = 0;
+        while (i < SZ(nums)) {
+            if (nums[i] == val)
+                nums.erase(nums.begin() + i);
+            else
+                i += 1;
+        }
+        return SZ(nums);
+    }
+};
+
+int main() {
+    return 0;
+}
