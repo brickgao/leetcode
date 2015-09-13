f29c4686ebf2a10fa7c5eb73f175787bfdbaa734
brickgao <brickgao@gmail.com>
2015-09-13 11:26:49 +0800

    add 'Remove Duplicates from Sorted List'

---
+++ src/algorithms/cpp/Remove_Duplicates_from_Sorted_List.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <cstdlib>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <set>
+using namespace std;
+#define out(v) cerr << #v << ": " << (v) << endl
+#define SZ(v) ((int)(v).size())
+const int maxint = -1u>>1;
+template <class T> bool get_max(T& a, const T &b) {return b > a? a = b, 1: 0;}
+template <class T> bool get_min(T& a, const T &b) {return b < a? a = b, 1: 0;}
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode *node = head, *pre = NULL;
+        int cnt;
+        while (node) {
+            if (pre && pre->val == node->val) {
+                pre->next = node->next;
+            }
+            else {
+                pre = node;
+            }
+            node = node->next;
+        }
+        return head;
+    }
+};
+
+int main() {
+    return 0;
+}
