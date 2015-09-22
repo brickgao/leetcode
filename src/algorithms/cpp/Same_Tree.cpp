01cdda3fb24985487d0b7a0333c52940a5c385ab
brickgao <brickgao@gmail.com>
2015-09-22 19:27:11 +0800

    add 'Same Tree'

---
+++ src/algorithms/cpp/Same_Tree.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <cstdlib>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <set>
+using namespace std;
+#define out(v) cerr << #v << ": " << (v) << endl
+#define SZ(v) ((int)(v).size())
+const int maxint = -1u>>1;
+template <class T> bool get_max(T& a, const T &b) {return b > a? a = b, 1: 0;}
+template <class T> bool get_min(T& a, const T &b) {return b < a? a = b, 1: 0;}
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (p == NULL || q == NULL) {
+            if (p == NULL && q == NULL)  return true;
+            else                         return false;
+        }
+        if (p->val != q->val)   return false;
+        return (this->isSameTree(p->left, q->left) && this->isSameTree(p->right, q->right));
+    }
+};
+
+int main() {
+    return 0;
+}
