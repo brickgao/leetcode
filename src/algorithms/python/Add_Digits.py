d31f36f7cbe6355ff1c4593550f9f424778276e7
brickgao <brickgao@gmail.com>
2015-09-23 10:54:24 +0800

    add 'Add Digits'

---
+++ src/algorithms/python/Add_Digits.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+import math
+
+
+class Solution(object):
+    def addDigits(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        if num == 0:
+            return 0
+        return num - 9 * ((num - 1) // 9)
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    print solution.addDigits(0)
+    print solution.addDigits(34)
