44af745f60c779ba66cdaa47420cce5c7faef63e
brickgao <brickgao@gmail.com>
2015-09-27 11:23:55 +0800

    add 'First Bad Version'

---
+++ src/algorithms/python/First_Bad_Version.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return a bool
+# def isBadVersion(version):
+
+class Solution(object):
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        l, r = 1, n
+        while l < r - 1:
+            mid = (l + r) >> 1
+            if isBadVersion(mid):
+                r = mid
+            else:
+                l = mid
+        if isBadVersion(l):
+            return l
+        if isBadVersion(r):
+            return r
