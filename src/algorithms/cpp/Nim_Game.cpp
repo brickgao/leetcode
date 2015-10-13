b7b5659b6625f7af4add3646d00944042454cce9
brickgao <brickgao@gmail.com>
2015-10-13 19:22:17 +0800

    add 'Nim Game'

---
+++ src/algorithms/cpp/Nim_Game.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <cstdlib>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <map>
+#include <set>
+using namespace std;
+#define out(v) cerr << #v << ": " << (v) << endl
+#define SZ(v) ((int)(v).size())
+const int maxint = -1u>>1;
+const double eps = 1e-6;
+template <class T> bool get_max(T& a, const T &b) {return b > a? a = b, 1: 0;}
+template <class T> bool get_min(T& a, const T &b) {return b < a? a = b, 1: 0;}
+
+class Solution {
+public:
+    bool canWinNim(int n) {
+        if (n % 4 == 0)     return false;
+        else                return true;
+    }
+};
+
+int main() {
+    return 0;
+}
