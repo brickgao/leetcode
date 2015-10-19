eb1b0603e5251ad218f55e0013e6c301c5211864
brickgao <brickgao@gmail.com>
2015-10-19 12:14:13 +0800

    add 'Single Number'

---
+++ src/algorithms/cpp/Single_Number.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <cstdlib>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <set>
+using namespace std;
+#define out(v) cerr << #v << ": " << (v) << endl
+#define SZ(v) ((int)(v).size())
+const int maxint = -1u>>1;
+template <class T> bool get_max(T& a, const T &b) {return b > a? a = b, 1: 0;}
+template <class T> bool get_min(T& a, const T &b) {return b < a? a = b, 1: 0;}
+
+class Solution {
+public:
+    int singleNumber(vector <int> &nums) {
+        int result = 0;
+        for (int &num: nums)
+            result ^= num;
+        return result;
+    }
+};
+
+int main() {
+    return 0;
+}
