edbf0229ba1aeebbfbc879492117856f33ffd9b0
brickgao <brickgao@gmail.com>
2015-10-23 18:51:24 +0800

    add 'Linked List Cycle II'

---
+++ src/algorithms/cpp/Linked_List_Cycle_II.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <cstdlib>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <set>
+using namespace std;
+#define out(v) cerr << #v << ": " << (v) << endl
+#define SZ(v) ((int)(v).size())
+const int maxint = -1u>>1;
+template <class T> bool get_max(T& a, const T &b) {return b > a? a = b, 1: 0;}
+template <class T> bool get_min(T& a, const T &b) {return b < a? a = b, 1: 0;}
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode *node1 = head, *node2 = head;
+        bool tag = false;
+        while (node1 && node2) {
+            node1 = node1->next;
+            if (node2->next)    node2 = node2->next->next;
+            else                node2 = node2->next;
+            if (node2 && node1 == node2) {
+                tag = true;
+                break;
+            }
+        }
+        if (tag) {
+            node1 = head;
+            while (node1 != node2)
+                node1 = node1->next, node2 = node2->next;
+            return node1;
+        }
+        else {
+            return NULL;
+        }
+    }
+};
+
+int main() {
+    return 0;
+}
