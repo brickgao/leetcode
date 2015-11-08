3ca9e608f87e37ee650eb0a799a6293ec760df3b
brickgao <brickgao@gmail.com>
2015-11-08 20:33:38 +0800

    add 'Reverse Bits'

---
+++ src/algorithms/cpp/Reverse_Bits.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <cstdlib>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <set>
+using namespace std;
+#define out(v) cerr << #v << ": " << (v) << endl
+#define SZ(v) ((int)(v).size())
+const int maxint = -1u>>1;
+template <class T> bool get_max(T& a, const T &b) {return b > a? a = b, 1: 0;}
+template <class T> bool get_min(T& a, const T &b) {return b < a? a = b, 1: 0;}
+
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t result = 0;
+        for (int i = 0; i < 32; ++ i) {
+            result <<= 1;
+            if (n & 1)  result |= 1;
+            n >>= 1;
+        }
+        return result;
+    }
+};
+
+int main() {
+    Solution solution = Solution();
+    cout << solution.reverseBits(43261596) << endl;
+    return 0;
+}
