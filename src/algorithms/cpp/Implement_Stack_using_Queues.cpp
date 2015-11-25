4c6f25d7c235891aacc85c070d9076ca1e7a20ff
brickgao <brickgao@gmail.com>
2015-11-25 14:06:22 +0800

    add 'Implement Stack using Queues'

---
+++ src/algorithms/cpp/Implement_Stack_using_Queues.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <cstdlib>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+using namespace std;
+#define out(v) cerr << #v << ": " << (v) << endl
+#define SZ(v) ((int)(v).size())
+const int maxint = -1u>>1;
+template <class T> bool get_max(T& a, const T &b) {return b > a? a = b, 1: 0;}
+template <class T> bool get_min(T& a, const T &b) {return b < a? a = b, 1: 0;}
+
+class Stack {
+private:
+    queue <int> q;
+
+public:
+    // Push element x onto stack.
+    void push(int x) {
+        q.push(x);
+    }
+
+    // Removes the element on top of the stack.
+    void pop() {
+        queue <int> q_new;
+        while (SZ(q) > 1) {
+            q_new.push(q.front());
+            q.pop();
+        }
+        q = q_new;
+    }
+
+    // Get the top element.
+    int top() {
+        return q.back();
+    }
+
+    // Return whether the stack is empty.
+    bool empty() {
+        return q.empty();
+    }
+};
+
+int main() {
+    return 0;
+}
