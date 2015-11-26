e781c2d4affe25df2b9805cb34990d15c7d1568f
brickgao <brickgao@gmail.com>
2015-11-26 14:06:43 +0800

    add 'Invert Binary Tree'

---
+++ src/algorithms/cpp/Invert_Binary_Tree.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <cstdlib>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <map>
+#include <set>
+using namespace std;
+#define out(v) cerr << #v << ": " << (v) << endl
+#define SZ(v) ((int)(v).size())
+const int maxint = -1u>>1;
+template <class T> bool get_max(T& a, const T &b) {return b > a? a = b, 1: 0;}
+template <class T> bool get_min(T& a, const T &b) {return b < a? a = b, 1: 0;}
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if (root == NULL)           return root;
+        swap(root->left, root->right);
+        if (root->left != NULL)     invertTree(root->left);
+        if (root->right != NULL)    invertTree(root->right);
+        return root;
+    }
+};
+
+int main() {
+    return 0;
+}
