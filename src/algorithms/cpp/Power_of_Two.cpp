28a206c55107a2a00ac4e6fa5ec8f8223e160919
brickgao <brickgao@gmail.com>
2015-11-29 11:49:10 +0800

    add 'Power of Two'

---
+++ src/algorithms/cpp/Power_of_Two.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <cstdlib>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <set>
+using namespace std;
+#define out(v) cerr << #v << ": " << (v) << endl
+#define SZ(v) ((int)(v).size())
+const int maxint = -1u>>1;
+template <class T> bool get_max(T& a, const T &b) {return b > a? a = b, 1: 0;}
+template <class T> bool get_min(T& a, const T &b) {return b < a? a = b, 1: 0;}
+
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        int cnt = 0;
+        while (n > 0) {
+            if (n & 1)      cnt += 1;
+            n >>= 1;
+        }
+        return cnt == 1;
+    }
+};
+
+int main() {
+    return 0;
+}
