e26c5872d81f384e3122a59dcead5cbdc5dce310
brickgao <brickgao@gmail.com>
2015-12-11 13:24:47 +0800

    add 'First Bad Version'

---
+++ src/algorithms/cpp/First_Bad_Version.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <cstdlib>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <set>
+using namespace std;
+#define out(v) cerr << #v << ": " << (v) << endl
+#define SZ(v) ((int)(v).size())
+const int maxint = -1u>>1;
+template <class T> bool get_max(T& a, const T &b) {return b > a? a = b, 1: 0;}
+template <class T> bool get_min(T& a, const T &b) {return b < a? a = b, 1: 0;}
+
+// Forward declaration of isBadVersion API.
+bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        long long l = 1, r = n;
+        while (l < r - 1) {
+            long long mid = (l + r) >> 1;
+            if (isBadVersion(mid))  r = mid;
+            else                    l = mid;
+        }
+        if (isBadVersion(l))    return l;
+        else                    return r;
+    }
+};
+
+int main() {
+    return 0; 
+}
