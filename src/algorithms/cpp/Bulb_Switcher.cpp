e8bec0869cc427d0320ae083cc8ba05b8f00daff
brickgao <brickgao@gmail.com>
2015-12-24 14:13:56 +0800

    add 'Bulb Switcher'

---
+++ src/algorithms/cpp/Bulb_Switcher.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <cstdlib>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <set>
+using namespace std;
+#define out(v) cerr << #v << ": " << (v) << endl
+#define SZ(v) ((int)(v).size())
+const int maxint = -1u>>1;
+template <class T> bool get_max(T& a, const T &b) {return b > a? a = b, 1: 0;}
+template <class T> bool get_min(T& a, const T &b) {return b < a? a = b, 1: 0;}
+
+class Solution {
+public:
+    int bulbSwitch(int n) {
+        return int(sqrt(n));
+    }
+};
+
+int main() {
+    return 0;
+}
