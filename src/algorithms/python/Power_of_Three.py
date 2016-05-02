40a328b722116d643bde7aad4448518f0f4969ef
brickgao <brickgao@gmail.com>
2016-05-02 13:25:58 +0800

    add 'Power of Three'

---
+++ src/algorithms/python/Power_of_Three.py
@@ -0,0 +1,12 @@
+#!/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+class Solution(object):
+    def isPowerOfThree(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        pow_of_three_limit = 1853020188851841
+        return False if n <= 0 or pow_of_three_limit % n != 0 else True
