c55892c27abcd6f23a86a76e4c42351695470459
brickgao <brickgao@gmail.com>
2016-06-01 12:16:24 +0800

    add 'Intersection of Two Arrays'

---
+++ src/algorithms/python/Intersection_of_Two_Arrays.py
@@ -0,0 +1,12 @@
+#!/bin/env python2
+# -*- coding: utf-8 -*-
+
+
+class Solution(object):
+    def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        return list(set(nums1) & set(nums2))
